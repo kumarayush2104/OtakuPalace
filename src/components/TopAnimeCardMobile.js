c3ebee5a373cdf8efeb970a503cd6d209654951d
Ayush Kumar <40270440+kumarayush2104@users.noreply.github.com>
2023-06-02 11:41:28 +0530

    TopAnimeSection: Final

--- src/components/TopAnimeCardMobile.js
+++ src/components/TopAnimeCardMobile.js
@@ -2,38 +2,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function TopAnimeCard(props) {
+export default function TopAnimeCardMobile(props) {
     return (
-        <section className="play-details">
+        <section className="play-details p-3">
             <div className="container p-lg-5 p-2">
-                <div className="row align-items-center">
-
+                <span className="badge bg-warning text-dark">Trending Now</span>
+                <div className="row align-items-center justify-content-center">
                     {/* Image Section Start */}
-                    <div className="col-md-3">
+                    <div className="col-md-3 col-5">
                         <div className="col-md-12 text-center">
-                            <div className="play-thumb">
+                            {/* Trending Now Banner Start */}
+
+                            {/* Trending Now Banner End */}
+                            <div className="play-thumb my-2">
                                 <img className="img-fluid" src={props.image} alt={props.title} />
                             </div>
                         </div>
                     </div>
                     {/* Image Section End */}
 
-                    <div className="col-md-9">
-
-                        {/* Trending Now Banner Start */}
-                        <span className="badge bg-warning text-dark">Trending Now</span>
-                        {/* Trending Now Banner End */}
-
+                    <div className="col-md-9 col-8">
                         <div className="play-details-content">
-
                             {/* Information Section Start */}
-                            <div className="title-block d-flex align-items-center justify-content-between">
-                                <h3 className="play-title">{props.title}</h3>
+                            <div className="title-block text-center my-3">
+                                <h3 className="play-title-mobile">{props.title}</h3>
                             </div>
                             <div className="movie-persons">
                                 <div className="person-block">
-                                    <h5 className="title">Genre</h5>
-                                    <p>
+                                    <h5 className="title m-1">Genre</h5>
+                                    <p className='d-flex flex-wrap col-7'>
                                         {props.genres.map((element) => (
                                             <span key={element} className="m-1">
                                                 {element}
@@ -47,7 +44,7 @@ export default function TopAnimeCard(props) {
                             {/* Button Section Start */}
                             <div className="details-buttons">
                                 <div className="row d-flex align-items-center">
-                                    <div className="col-6 col-xl mb-xl-0 mb-3">
+                                    <div className="col-12 col-xl mb-xl-0 mb-3">
                                         <Link to={`/View/${props.id}`} className="btn d-block hvr-sweep-to-right" tabIndex="0">
                                             <i className="icofont-ui-play mr-2" aria-hidden="true"></i>Play
                                         </Link>
---
+++ src/components/TopAnimeSection.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react'
+import TopAnimeCard from './TopAnimeCard'
+import { useLocation } from 'react-router-dom';
+import LoadingAnimation from './LoadingAnimation';
+import ReactOwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import AnimeBanner from '../assets/AnimeBanner2.jpg'
+import TopAnimeCardMobile from './TopAnimeCardMobile';
+
+export default function TopAnimeSection(props) {
+
+    const [isError, setError] = useState(false);
+    const animeId = useLocation();
+    const [animeList, setAnimeList] = useState(null);
+
+    const CarouselOptions = {
+        loop: true,
+        autoplay: true,
+        items: 1,
+        dots: false,
+    };
+
+    // Handle Errors
+    const handleError = () => {
+        setError(true);
+    }
+
+    useEffect(() => {
+        // Fetches Anime Information
+        setAnimeList(null);
+        const fetchData = async () => {
+            try {
+                const response = await fetch("https://api.consumet.org/meta/anilist/trending");
+                if (response.ok) {
+                    const data = await response.json();
+                    setAnimeList(data.results);
+                } else {
+                    handleError();
+                }
+            } catch (error) {
+                handleError();
+                console.error(error);
+            }
+        };
+        fetchData();
+    }, [animeId, props.largeScreen]);
+
+    return (
+        <>
+            {isError ? <h1 className='text-white text-center p-5'>Message From Top Banner: <br /><br /><i className="fa-solid fa-warning" style={{ color: "#FF0000" }} /> Failed to Fetch Data</h1> : animeList ?
+                <div className="container-flex banner" style={{ backgroundImage: `url(${AnimeBanner})` }}>
+                    <ReactOwlCarousel className="owl-carousel owl-theme owl-loaded owl-drag" {...CarouselOptions}>
+                        {animeList.map((element) => props.largeScreen ? <TopAnimeCard
+                            id={element.id}
+                            image={element.image}
+                            description={element.description}
+                            genres={element.genres}
+                            title={element.title.english ? element.title.english : element.title.romaji} /> :
+
+                            <TopAnimeCardMobile
+                                id={element.id}
+                                image={element.image}
+                                description={element.description}
+                                genres={element.genres}
+                                title={element.title.english ? element.title.english : element.title.romaji} />)}
+                    </ReactOwlCarousel>
+                </div> : <LoadingAnimation />}
+
+        </>
+    )
+}
